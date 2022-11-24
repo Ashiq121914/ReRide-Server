0cb0d4d420cb21e1ffecba3628ff919eb4b05e7c
GaziAshiq <ashiq121914@gmail.com>
2022-11-24 21:05:42 +0600

    connect to the database and fetch categories

--- index.js
+++ index.js
@@ -5,9 +5,37 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+require("dotenv").config();
+
 //middle were
 app.use(cors());
 app.use(express.json());
+
+//database connection
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kryxy3e.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    const categoryCollection = client.db("ReRide").collection("categories");
+
+    //categories
+    app.get("/categories", async (req, res) => {
+      const query = {};
+      const categories = await categoryCollection.find(query).toArray();
+      res.send(categories);
+    });
+  } finally {
+  }
+}
+
+run().catch(console.log);
 
 app.get("/", async (req, res) => {
   res.send("Resale webite server is running");
