ff2328204e1652392f7b253aa1ec0f7b523b6efa
GaziAshiq <ashiq121914@gmail.com>
2022-11-26 21:34:32 +0600

    added jwt and bookings for my order

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 5000;
 
@@ -20,6 +21,25 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
+console.log(process.env.ACCESS_TOKEN);
+
+//jwt function
+function verifyJWT(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    return res.status(401).send("unauthorized access");
+  }
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "forbidden access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
 
 async function run() {
   try {
@@ -29,6 +49,22 @@ async function run() {
       .collection("allCategoryProducts");
     const usersCollection = client.db("ReRide").collection("users");
     const bookingsCollection = client.db("ReRide").collection("bookings");
+
+    //jwt
+    app.get("/jwt", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "2d",
+        });
+        return res.send({ accessToken: token });
+      }
+
+      res.status(403).send({ accessToken: " " });
+    });
 
     //categories
     app.get("/categories", async (req, res) => {
@@ -71,6 +107,21 @@ async function run() {
         .find(query)
         .toArray();
       res.send(CategoryProducts);
+    });
+
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const email = req.query.email;
+
+      const decodedEmail = req.decoded.email;
+      console.log(decodedEmail);
+
+      if (email !== decodedEmail) {
+        return res.status(403).send({ message: "forbidden access" });
+      }
+      const query = { userEmail: email };
+
+      const bookings = await bookingsCollection.find(query).toArray();
+      res.send(bookings);
     });
 
     app.post("/users", async (req, res) => {
