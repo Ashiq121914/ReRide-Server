baba7685ea9bfb227abe87c703d6f01110765c4a
GaziAshiq <ashiq121914@gmail.com>
2022-11-28 22:52:35 +0600

    last changes with correction

--- index.js
+++ index.js
@@ -165,14 +165,7 @@ async function run() {
 
     app.get("/bookings", verifyJWT, async (req, res) => {
       const email = req.query.email;
-
       
-
-      const decodedEmail = req.decoded.email;
-
-      if (email !== decodedEmail) {
-        return res.status(403).send({ message: "forbidden access" });
-      }
       const query = { userEmail: email };
 
       const bookings = await bookingsCollection.find(query).toArray();
